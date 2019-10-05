f5e3ab6604d89b27225bdf55639292e7b5dfd58a
Majda Osmic <mobearette@gmail.com>
2019-10-05 12:00:00 +0200

    Old way for read line

--- AsyncStreams/Program.cs
+++ AsyncStreams/Program.cs
@@ -9,14 +9,13 @@ namespace AsyncStreams
     {
         public static async Task Main(string[] args) //Main can be async since C#7
         {
-            await foreach (var line in ReadLines())
+            await foreach (var line in ReadLinesAsync())
             {
                 Console.WriteLine(line);
             }
         }
 
-
-        static async IAsyncEnumerable<string> ReadLines()
+        static async IAsyncEnumerable<string> ReadLinesAsync()
         {
             using var reader = new StreamReader(@"C:\Users\u12r23\Desktop\LoremIpsum"); //new in C#8
             while (!reader.EndOfStream)
@@ -24,5 +23,31 @@ namespace AsyncStreams
                 yield return await reader.ReadLineAsync();
             }
         }
+
+        #region old way
+
+        public static void WriteToConsole()
+        {
+            foreach (var line in ReadLines())
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+
+        static IEnumerable<string> ReadLines()
+        {
+            using (var reader = new StreamReader(@"C:\Users\u12r23\Desktop\LoremIpsum"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    yield return reader.ReadLine();
+                }
+            }
+        }
+
+        #endregion
     }
+
+
 }
